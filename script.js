27b8a29dacb8aef5075f9735a7eecabcb1ac9740
Andrew Murray <atmurray@bellsouth.net>
2020-04-08 21:12:01 -0400

    added coin history ajax

--- script.js
+++ script.js
@@ -3,25 +3,47 @@
 var apiKey = null;
 var cryptCoin =null;
 var exchangeCurrency= 'PHP';
+var coinID='1'
 var exQuery = 'https://api.exchangeratesapi.io/latest?base=USD&symbols=USD,'+exchangeCurrency;
 var cryptoQuery = 'https://api.coinranking.com/v1/public/coins'
-
+var historyQuery= 'https://api.coinranking.com/v1/public/coin/'+coinID+'?base=USD&timePeriod=7d'
 ///////// get exchange rates for coins  ////////
 
-$.ajax({ 
-    url: exQuery,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
+// $.ajax({ 
+//     url: exQuery,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
     
-});
+// });
 
 ///////get cryptocoin pricing base USD  ///////////
 $.ajax({ 
     url: cryptoQuery,
     method: "GET"
 }).then(function (response) {
+    var result =response;
+    var len = result.data.coins.length;
+    console.log(result)
+    for(let i =0; i < len; i++){
+        console.log(result.data.coins[i].id);
+        console.log(result.data.coins[i].name);
+        console.log(result.data.coins[i].symbol);
+    }
+
+    
+});
+
+///////get cryptocoin price history by 7day periods//////
+
+$.ajax({ 
+    url: historyQuery,
+    method: "GET"
+}).then(function (response) {
+    var result =response;
     console.log(response);
-    console.log(response.data.coins.length)
+    
+    
+
     
 });
