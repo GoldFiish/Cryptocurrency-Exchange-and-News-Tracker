66799781279543e251255f6ee54b02fc0d8972ff
mazdamike <mwpowell44@gmail.com>
2020-04-10 22:25:47 -0400

    feat: fetches and display three news articles

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-// Sample api key, variable for type of currency, and query URL
-var apiKey = "33a2934dfaa04b2b817eb3096ee6754e"
-var cryptoCurrency = "ethereum";
-var queryURL = "https://newsapi.org/v2/everything?q=crypto-AND-" + cryptoCurrency + "&language=en&pageSize=3&sortBy=publishedAt&apiKey=" + apiKey;
+// On-click functiion to get and display news articles
+$("button").on("click", function (event) {
+  event.preventDefault();
 
-// AJAX call to news api
-$.ajax({
+  var cryptoCurrency = $("#crypto-opt").val();
+  var apiKey = "33a2934dfaa04b2b817eb3096ee6754e"
+  var queryURL = "https://newsapi.org/v2/everything?q=crypto-AND-" + cryptoCurrency + "&language=en&pageSize=3&sortBy=publishedAt&apiKey=" + apiKey;
+
+  // AJAX call to news api
+  $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-    console.log(response);
+  }).then(function (response) {
+
+    for (var i = 0; i < response.articles.length; i++) {
+
+      var title = response.articles[i].title;
+      $("#title" + i).text(title);
+
+      var author = response.articles[i].author;
+      $("#author" + i).text(author);
+
+      var source = response.articles[i].source.name;
+      $("#source" + i).text(source);
+
+      var pubDate = response.articles[i].publishedAt;
+      $("#pubDate" + i).text(pubDate);
+
+      var article = response.articles[i].content;
+      $("#article" + i).text(article);
+    }
   });
+});
