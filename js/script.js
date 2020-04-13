c47a6d9df7f9e2b09e04bc457551b0bdab38f18d
Esther Kwon <ekholic7@gmail.com>
2020-04-13 01:59:30 -0400

    updated hmtl,css,js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// hamburger icon function
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+// On-click functiion to get and display news articles
+$("button").on("click", function (event) {
+  event.preventDefault();
+
+  var cryptoCurrency = $("#crypto-opt").val();
+  var apiKey = "33a2934dfaa04b2b817eb3096ee6754e"
+  var queryURL = "https://newsapi.org/v2/everything?q=crypto-AND-" + cryptoCurrency + "&language=en&pageSize=3&sortBy=publishedAt&apiKey=" + apiKey;
+
+  // AJAX call to news api
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    // Loop through the array and display three articles
+    for (var i = 0; i < response.articles.length; i++) {
+
+      var title = response.articles[i].title;
+      $("#title" + i).text(title);
+
+      var author = response.articles[i].author;
+      $("#author" + i).text(author);
+
+      var source = response.articles[i].source.name;
+      $("#source" + i).text(source);
+
+      var pubDate = response.articles[i].publishedAt;
+      $("#pubDate" + i).text(pubDate);
+
+      var article = response.articles[i].content;
+      $("#article" + i).text(article);
+    }
+  });
+});
