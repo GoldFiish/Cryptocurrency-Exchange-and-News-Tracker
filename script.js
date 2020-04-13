f701dc70d9e601d7dbffd416b5197a4e8aa05c4c
Andrew Murray <atmurray@bellsouth.net>
2020-04-13 19:02:14 -0400

    changing input default values to 0

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 var apiKey = null;
 var cryptCoin = null;
 var exchangeCurrency = 'PHP';
-var coinID = null;
+var coinID = '1';
 var coinName = null;
 var coinSymbol = null;
 var coinPrice = null;
